28afa2aeafbbb6bfec08777e6dab563bafb49eed
cinc98 <ivan.cincurak98@gmail.com>
2020-11-11 18:58:43 +0100

    added front

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from flask import Flask, request
-
+from flask import Flask, request, render_template, url_for
+import os
+import random 
 app = Flask(__name__)
 
-@app.route('/model/<string:input_text>', methods = ['GET'])
-def modelPredict(input_text):
-    text_for_model = input_text
+@app.route('/predict', methods = ['POST'])
+def modelPredict():
+    sentence = request.form['sentence']
     # Model predicts sentiment for text
     
-    sentiment = ""
-    ret_value = { "sentiment" : text_for_model}
-    return ret_value,200
+    return render_template('index.html', sentence = sentence, predict = str(random.randint(0, 1)))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
